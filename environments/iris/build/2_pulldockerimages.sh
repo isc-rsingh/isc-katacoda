47e5c2252c6c2c52069587b19e11f010114c27ab
Robert Kuszewski <rkuszews@USMBP13rkuszews.local>
2021-01-04 15:06:24 -0500

    follow example directly

---
+++ environments/iris/build/2_pulldockerimages.sh
@@ -0,0 +1,2 @@
+echo "Pulling container..."
+docker pull containers.intersystems.com/intersystems/iris-community:2020.4.0.524.0
--- environments/iris/build/4_configure_environment.sh
+++ environments/iris/build/4_configure_environment.sh
@@ -2,7 +2,7 @@ cat << EOF > /opt/configure-environment.sh
 #!/bin/bash
 
 echo "Starting container..."
-docker run --name iris -d --publish 1972:1972 --publish 52773:52773 --cap-add IPC_LOCK $container_image
+docker run --name iris -d --publish 1972:1972 --publish 52773:52773 --cap-add IPC_LOCK containers.intersystems.com/intersystems/iris-community:2020.4.0.524.0
 
 EOF
 chmod +x /opt/configure-environment.sh
