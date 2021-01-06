4b075cbce3b8b72369b7fa1e23130cd484c2d3c5
Robert Kuszewski <rkuszews@au7490dmoutia.iscinternal.com>
2021-01-06 16:27:27 -0500

    add suggested changes

--- environments/iris-running/build/1_installpackages.sh
+++ environments/iris-running/build/1_installpackages.sh
@@ -1,2 +1,3 @@
-apt-get update
-apt-get install -y vim
+systemctl stop docker
+sed -i -e 's/"storage-driver": "overlay",/"storage-driver": "overlay2",/' /etc/docker/daemon.json
+systemctl start docker
