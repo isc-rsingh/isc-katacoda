3e91d4f2354d73417629854acd6c03191c15c6fb
Raj Singh <3768695+isc-rsingh@users.noreply.github.com>
2021-01-08 22:54:34 -0500

    vscode extension must be named that

--- environments/vscodeenv/build/1_installpackages.sh
+++ environments/vscodeenv/build/1_installpackages.sh
@@ -5,9 +5,9 @@
 # cd /tmp && apt install bsdtar -y
 apt install bsdtar -y
 
-curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/intersystems-community/vsextensions/vscode-objectscript/1.0.6/vspackage | bsdtar -xvf - intersystems-community.vscode-objectscript-1.0.6
+curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/intersystems-community/vsextensions/vscode-objectscript/1.0.6/vspackage | bsdtar -xvf - extension
 
-mv intersystems-community.vscode-objectscript-1.0.6 /opt/.katacodacode/extensions/
+mv extension /opt/.katacodacode/extensions/intersystems-community.vscode-objectscript-1.0.6
 
 mkdir -p /opt/.katacodacode/user-data/User/
 cat << VSCODEEOF > /opt/.katacodacode/user-data/User/settings.json
