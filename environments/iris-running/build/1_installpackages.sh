83d652c858772aadd0927d46c94dc70d684ca613
Raj Singh <3768695+isc-rsingh@users.noreply.github.com>
2021-01-11 17:48:22 -0500

    add VS Code ObjectScript stuff

--- environments/iris-running/build/1_installpackages.sh
+++ environments/iris-running/build/1_installpackages.sh
@@ -1,3 +1,44 @@
+#!/bin/bash
+
 systemctl stop docker
 sed -i -e 's/"storage-driver": "overlay",/"storage-driver": "overlay2",/' /etc/docker/daemon.json
 systemctl start docker
+
+# cd /tmp && apt install bsdtar -y
+apt install bsdtar -y
+
+# Install ObjectScript Extension
+curl -L https://github.com/intersystems-community/vscode-objectscript/releases/download/v1.0.8-beta.3/vscode-objectscript-1.0.8-beta.3.vsix | bsdtar -xvf - extension
+mv extension /opt/.katacodacode/extensions/intersystems-community.vscode-objectscript-1.0.8-beta.3
+
+# Install Language Server Extension
+# curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/intersystems/vsextensions/language-server/1.0.6/vspackage | bsdtar -xvf - extension
+# mv extension /opt/.katacodacode/extensions/intersystems.language-server-1.0.6
+
+mkdir -p /opt/.katacodacode/user-data/User/
+cat << VSCODEEOF > /opt/.katacodacode/user-data/User/settings.json
+{ 
+  "objectscript.conn": {
+    "active": true,
+    "host": "localhost",
+    "port": 52773,
+    "username": "SYSTEM",
+    "password": "_SYS",
+    "ns": "USER",
+    "https": true
+  },
+  "workbench.startupEditor": "none", 
+  "files.autoSave": "on",
+  "editor.minimap.enabled": false,
+  "window.autoDetectColorScheme": false,
+  "workbench.colorCustomizations": {},
+  "workbench.colorTheme": "Default Dark+",
+  "editor.semanticHighlighting.enabled": false,
+  "editor.tokenColorCustomizations": {
+      "semanticHighlighting": true
+  },
+  "files.exclude": {
+    "**/.*": true
+  }
+}
+VSCODEEOF
