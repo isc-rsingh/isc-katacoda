799d66c1c45433e4dcc53479dd6aeab49b942c4c
Raj Singh <3768695+isc-rsingh@users.noreply.github.com>
2021-01-11 14:50:06 -0500

    try v1.0.8beta3

--- environments/vscodeenv/build/1_installpackages.sh
+++ environments/vscodeenv/build/1_installpackages.sh
@@ -3,9 +3,10 @@
 # cd /tmp && apt install bsdtar -y
 apt install bsdtar -y
 
-curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/intersystems-community/vsextensions/vscode-objectscript/1.0.7/vspackage | bsdtar -xvf - extension
-
-mv extension /opt/.katacodacode/extensions/intersystems-community.vscode-objectscript-1.0.7
+# curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/intersystems-community/vsextensions/vscode-objectscript/1.0.7/vspackage | bsdtar -xvf - extension
+# mv extension /opt/.katacodacode/extensions/intersystems-community.vscode-objectscript-1.0.7
+curl -L https://github.com/intersystems-community/vscode-objectscript/releases/download/v1.0.8-beta.3/vscode-objectscript-1.0.8-beta.3.vsix | bsdtar -xvf - extension
+mv extension /opt/.katacodacode/extensions/intersystems-community.vscode-objectscript-1.0.8-beta.3
 
 mkdir -p /opt/.katacodacode/user-data/User/
 cat << VSCODEEOF > /opt/.katacodacode/user-data/User/settings.json
