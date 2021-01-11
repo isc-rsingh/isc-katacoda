a252fb47216b3708388907628ee592cda3fb9171
Raj Singh <3768695+isc-rsingh@users.noreply.github.com>
2021-01-11 15:09:34 -0500

    add language server 1.0.6

--- environments/vscodeenv/build/1_installpackages.sh
+++ environments/vscodeenv/build/1_installpackages.sh
@@ -3,10 +3,15 @@
 # cd /tmp && apt install bsdtar -y
 apt install bsdtar -y
 
+# Install ObjectScript Extension
 # curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/intersystems-community/vsextensions/vscode-objectscript/1.0.7/vspackage | bsdtar -xvf - extension
 # mv extension /opt/.katacodacode/extensions/intersystems-community.vscode-objectscript-1.0.7
 curl -L https://github.com/intersystems-community/vscode-objectscript/releases/download/v1.0.8-beta.3/vscode-objectscript-1.0.8-beta.3.vsix | bsdtar -xvf - extension
 mv extension /opt/.katacodacode/extensions/intersystems-community.vscode-objectscript-1.0.8-beta.3
+
+# Install Language Server Extension
+curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/intersystems/vsextensions/language-server/1.0.6/vspackage | bsdtar -xvf - extension
+mv extension /opt/.katacodacode/extensions/intersystems.language-server-1.0.6
 
 mkdir -p /opt/.katacodacode/user-data/User/
 cat << VSCODEEOF > /opt/.katacodacode/user-data/User/settings.json
